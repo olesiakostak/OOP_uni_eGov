064aeea67e0019c188a68da0b47b47857db08b39
Olesia Kostak <olesiakostak@.com>
2025-03-01 23:37:00 +0200

    Thanks to Michael

--- lab_1/lab_1_olesia_kostak_e_gov.cpp
+++ lab_1/lab_1_olesia_kostak_e_gov.cpp
@@ -4,21 +4,42 @@ using namespace std;
 
 class Citizen
 {
-private:
+protected:
     string name;
     int age;
 
 public:
-
+    Citizen(string user_name, int user_age)
+    {
+        name = user_name;
+        age = user_age;
+    }
+    void ShowUserInformation()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl; 
+    }
 };
 
-class Student: public Citizen {};
+class Student: public Citizen 
+{
+private:
+
+public:
+    using Citizen::Citizen;
+};
 class Employee: public Citizen {};
 class Entrepreneur: public Citizen {};
 
 int main()
 {
-
-
+    cout << "You're welcome to our e-Government" << endl << endl << "Choose an option to continue: " << endl;
+    cout << "1. Register \n 2. "; 
+    while (true)
+    {
+        break;
+    }
+    Student ct("f", 3);
+    ct.ShowUserInformation();
     return 0;
 }
