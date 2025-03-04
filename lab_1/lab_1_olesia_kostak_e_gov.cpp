49072e2fba93ee5c5edbffaa7208e224bdd45f80
Olesia Kostak <olesiakostak@.com>
2025-03-04 20:03:32 +0200

    #05

--- lab_1/lab_1_olesia_kostak_e_gov.cpp
+++ lab_1/lab_1_olesia_kostak_e_gov.cpp
@@ -5,20 +5,25 @@ using namespace std;
 class Citizen
 {
 protected:
-    string name;
-    int age;
+    string filename = "user_data.txt";
+
+    struct User
+    {
+        int age;
+        string name;
+        string surname;
+        string profession;
+    };
+    User user;
 
 public:
     Citizen(string user_name, int user_age)
     {
-        name = user_name;
-        age = user_age;
+        user.name = user_name;
+        user.age = user_age;
     }
-    void ShowUserInformation()
-    {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl; 
-    }
+    void Register();
+    void ShowUserInformation();
 };
 
 class Student: public Citizen 
@@ -33,13 +38,52 @@ class Entrepreneur: public Citizen {};
 
 int main()
 {
-    cout << "You're welcome to our e-Government" << endl << endl << "Choose an option to continue: " << endl;
-    cout << "1. Register \n 2. "; 
+    cout << "You're welcome to our e-Government" << endl << endl;
+    int user_choice;
     while (true)
     {
+        cout << "Choose an option to continue (enter a number): " << endl;
+        cout << "1. Register \n 2. Find out social assistance \n 3. Calculate taxes \n 4. Exit \n"; 
+        cin >> user_choice;
+        switch (user_choice)
+        {
+        case '1':
+            /* code */
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        default:
+            cout << "You have entered not correct answer.";
+            break;
+        }
         break;
     }
     Student ct("f", 3);
     ct.ShowUserInformation();
     return 0;
+}
+
+void Citizen::ShowUserInformation()
+{
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl; 
+}
+void Citizen::Register()
+{
+    cout << "Enter your \n Name: "; 
+    cin >> name;
+    cout << "\n Surname: ";
+    cin >> surname;
+    cout << "\n Age: ";
+    cin >> age;
+    cout << "\n Profession: ";
+    cin >> progession;
+
+    cin >> 
+
+
 }
