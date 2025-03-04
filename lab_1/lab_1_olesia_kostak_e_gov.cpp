88a54d241bfada67d18d0c0b9e2d2476e49ef31f
Olesia Kostak <olesiakostak@.com>
2025-03-05 00:07:13 +0200

    #04

--- lab_1/lab_1_olesia_kostak_e_gov.cpp
+++ lab_1/lab_1_olesia_kostak_e_gov.cpp
@@ -6,11 +6,12 @@ class Citizen
 {
 protected:
     string filename = "user_data.txt";
-
+    int social_assistance = 0;
     struct User
     {
         int age;
         int work_experience;
+        int salary;
         string name;
         string surname;
         string profession;
@@ -27,16 +28,19 @@ class Student: public Citizen
 {
 public:
     using Citizen::Citizen;
+    int CalculateSocialAssistance();
 };
 class Employee: public Citizen 
 {
 public:
     using Citizen::Citizen;
+    int CalculateSocialAssistance();
 };
 class Entrepreneur: public Citizen 
 {
 public:
     using Citizen::Citizen;
+    int CalculateSocialAssistance();
 };
 
 
@@ -45,6 +49,7 @@ public:
 int main()
 {
     Citizen* user = nullptr;
+    string citizen_categories[] = {"student", "employee", "entrepreneur"};
     cout << "You're welcome to our e-Government" << endl << endl;
     int user_choice;
     while (true)
@@ -111,13 +116,47 @@ void Citizen::Register()
     cin >> current_user.age;
     cout << "Profession: ";
     cin >> current_user.profession;
+    cout << "Salary: ";
+    cin >> current_user.salary;
     cout << "Work experience: ";
     cin >> current_user.work_experience;
 }
 
+int Student::CalculateSocialAssistance()
+{
+    cout << "Enter your degree:\n 1. Associate degree  2. Bachelor`s degree   3. Master`s degree  4. Doctoral degree  5. Professional degree\n";
+    int degree;
+    cin >> degree;
+    switch (degree)
+    {
+    case 1:
+        social_assistance += 35;
+        break;
+    case 2:
+        social_assistance += 55;
+        break;
+    case 3:
+        social_assistance += 70;
+        break;
+    case 4:
+        social_assistance += 100;
+        break;
+    case 5:
+        social_assistance += 150;
+        break;
+    default:
+        cout << "You have entered not correct option" << endl;
+        break;
+    }
+    cout << social_assistance << '$' << endl;
+}
 
-// current_user.age = user_age;
-//         current_user.work_experience = user_work_experience;
-//         current_user.name = user_name;
-//         current_user.surname = user_surname;
-//         current_user.profession = user_profession;   
+int Employee::CalculateSocialAssistance()
+{
+
+}
+
+int Entrepreneur::CalculateSocialAssistance()
+{
+
+}
