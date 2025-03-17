618684ce4b9bd16821526508c5f37b57e77bd296
Olesia Kostak <olesiakostak@.com>
2025-03-17 17:50:56 +0200

    #05

--- lab_2/eGov/src/Models/Citizen.cs
+++ lab_2/eGov/src/Models/Citizen.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Drawing;
+using System.Dynamic;
+using System.Security.AccessControl;
 using eGov.Interfaces;
 
 namespace eGov.Models
@@ -10,26 +12,40 @@ namespace eGov.Models
         private int age { get; set; }
         private ITaxPayer? tax_payer { get; set; }
         private IDriver? car_driver { get; set; }
+        private Adress? home_address { get; set; }
 
-        public Citizen() {}
+        public Citizen() 
+        {
+            home_address = null;
+        }
 
         public void InputInfo ()
         {
             Console.Write("Enter full name: ");
             name = Console.ReadLine();
+
             Console.Write("Enter age: ");
             age = int.Parse(Console.ReadLine());
+
             Console.WriteLine("Are you tax payer? (y/n): ");
             char user_input = char.ToLower(Console.ReadKey(true).KeyChar);
             if (user_input == 'y')
             {
                 tax_payer = new TaxPayer();
             }
+
             Console.WriteLine("Are you car driver? (y/n): ");
             user_input = char.ToLower(Console.ReadKey(true).KeyChar);
             if (user_input == 'y')
             {
                 car_driver = new CarDriver();
+            }
+
+            Console.WriteLine("Do you want to enter the place of recidence? (optional) (y/n)");
+            user_input = char.ToLower(Console.ReadKey(true).KeyChar);
+            if (user_input == 'y')
+            {
+                InputAddress();
             }
         }
 
@@ -62,13 +78,24 @@ namespace eGov.Models
             Console.ResetColor();
         }
 
+        public Adress? GetAddress()
+        {
+            return home_address;
+        }
+
+        public void InputAddress()
+        {
+            Adress home_adress  = new Adress();
+            home_adress.InputAddress();
+        }
+
         public class Adress
         {
             private string country { get; set; }
             private string city { get; set; }
             private string street {get; set; }
 
-            public void InputAdress()
+            public void InputAddress()
             {
                 Console.Write("Input country: ");
                 country = Console.ReadLine();
@@ -78,7 +105,7 @@ namespace eGov.Models
                 street = Console.ReadLine();
             }
 
-            public void ShowAdress()
+            public void ShowAddress()
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
 
