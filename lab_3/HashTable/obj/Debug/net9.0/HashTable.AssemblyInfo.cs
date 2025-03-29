4c21e2a3f84f1467188388a92d3586a917dff059
Olesia Kostak <Olesiakostak@gmail.com>
2025-03-29 11:21:44 +0200

    #02

--- lab_3/HashTable/obj/Debug/net9.0/HashTable.AssemblyInfo.cs
+++ lab_3/HashTable/obj/Debug/net9.0/HashTable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HashTable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff52ddf4f564ff55d8e7b792b648e30dbecca23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07187f43cfa92971b66d537b375503c0c2337b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("HashTable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HashTable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lab_3/HashTable/src/HashTable.cs
+++ lab_3/HashTable/src/HashTable.cs
@@ -21,5 +21,6 @@ namespace HashTable.src
             return key;
         }
         
+        
     }
 }
